537fded67ca4431768c46b6aa3b986d8bd034956
SmarTest Demo User <demo@localhost.localdomain>
2021-11-23 16:38:16 +0800

    2021/11/23
    Change-Id: If86db5aaaa905a4832da693d1f14136fa8653d80

--- src/main/java/org/option/binarysearch/BinarySearch.java
+++ src/main/java/org/option/binarysearch/BinarySearch.java
@@ -1,31 +1,32 @@
 package org.option.binarysearch;
 
 /**
- * @author Timo
+ * @author Jun
  * @date 2021/11/22
  * @leetcode 704. Binary Search
  */
 public class BinarySearch {
     public int search(int[] nums, int target) {
 
-        int length, head, tail, referencePoint = 0, reference = 0;
+        int length, head, tail, referencePoint, reference;
         head = 0;
         tail = nums.length - 1;
         do {
             length = tail - head + 1;
             if (length % 2 != 0) {
                 reference = nums[referencePoint = (head + tail) / 2];
+                if (target == reference) {
+                    return referencePoint;
+                } else if (target < reference) {
+                    tail = referencePoint;
+                } else {
+                    head = referencePoint;
+                }
             } else {
-                if (target == nums[++head]) {
+                if (target == nums[head]) {
                     return head;
                 }
-            }
-            if (target == reference) {
-                return referencePoint;
-            } else if (target < reference) {
-                tail = referencePoint;
-            } else if (target > reference) {
-                head = referencePoint;
+                head++;
             }
             if (head == tail && nums[head] != target) {
                 head = -1;
