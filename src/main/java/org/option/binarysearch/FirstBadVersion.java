6ebcfaa1f09027ad3040bca4266922b6743414d2
SmarTest Demo User <demo@localhost.localdomain>
2021-11-24 14:19:37 +0800

    2021/11/24_1
    Change-Id: I57f1dd8fb673244de0514d92a4c7952a4567a700

--- src/main/java/org/option/binarysearch/FirstBadVersion.java
+++ src/main/java/org/option/binarysearch/FirstBadVersion.java
@@ -7,23 +7,45 @@ package org.option.binarysearch;
  */
 public class FirstBadVersion {
     public int firstBadVersion(int n) {
-        int left, right, referencePoint;
-        left = 0;
-        right = n - 1;
+        int left, right, reference = 0;
+        left = 1;
+        right = n;
         while (left <= right) {
-            referencePoint = left + ((right - left) >> 1);
-            if (isBadVersion(referencePoint + 1)) {
-                if (!isBadVersion(referencePoint)) {
-                    return referencePoint;
-                } else 
-
-            } else ()
+            reference = left + ((right - left) >> 1);
+            if (isBadVersion(reference)) {
+                if (!isBadVersion(reference - 1) || reference == 1) {
+                    return reference;
+                } else {
+                    right = reference - 1;
+                }
+            } else {
+                if (isBadVersion(reference + 1)) {
+                    return reference + 1;
+                } else {
+                    left = reference + 1;
+                }
+            }
         }
+        return reference + 1;
+    }
 
-
+    public int officialFirstBadVersion(int n) {
+        int left, right, reference;
+        left = 1;
+        right = n;
+        while (left < right) {
+            reference = left + ((right - left) >> 1);
+            if (isBadVersion(reference)) {
+                right = reference;
+            } else {
+                left = reference + 1;
+            }
+        }
+        return left;
     }
 
     public boolean isBadVersion(int version) {
         return true;
     }
+
 }
--- src/test/java/org/option/AppTest.java
+++ src/test/java/org/option/AppTest.java
@@ -4,11 +4,11 @@ import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
 import org.option.binarysearch.BinarySearch;
+import org.option.binarysearch.FirstBadVersion;
 
 public class AppTest {
     @Test
     public void test() {
-        System.out.println(10%3);
-        System.out.println(1/2);
+        int[] i = {1, 2, 3, 4, 4, 4, 4, 4, 4};
     }
 }
