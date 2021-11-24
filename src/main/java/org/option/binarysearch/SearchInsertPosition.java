4f10f5a055d2dc96495abec2802e75b8c5f35549
SmarTest Demo User <demo@localhost.localdomain>
2021-11-24 17:53:52 +0800

    2021/11/24_2
    Change-Id: I5528b52a510ce2d9f2f107374b6b8071b5c0969f

---
+++ src/main/java/org/option/binarysearch/SearchInsertPosition.java
@@ -0,0 +1,42 @@
+package org.option.binarysearch;
+
+/**
+ * @author Jun
+ * @date 2021/11/24
+ * @leetcode 35. Search Insert Position
+ */
+public class SearchInsertPosition {
+    public int searchInsert(int[] nums, int target) {
+        int left, right, reference, referencePoint;
+        left = 0;
+        right = nums.length - 1;
+        while (left < right) {
+            referencePoint = left + ((right - left) >> 1);
+            reference = nums[referencePoint];
+            if (target == reference) {
+                return referencePoint;
+            } else if (target < reference) {
+                right = referencePoint - 1;
+            } else {
+                left = referencePoint + 1;
+            }
+        }
+        return target > nums[left] ? ++left : left;
+
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- src/test/java/org/option/AppTest.java
+++ src/test/java/org/option/AppTest.java
@@ -5,10 +5,13 @@ import static org.junit.Assert.assertTrue;
 import org.junit.Test;
 import org.option.binarysearch.BinarySearch;
 import org.option.binarysearch.FirstBadVersion;
+import org.option.binarysearch.SearchInsertPosition;
 
 public class AppTest {
     @Test
     public void test() {
-        int[] i = {1, 2, 3, 4, 4, 4, 4, 4, 4};
+        int[] i = {1, 3, 5, 7, 9};
+        int[] j = {1};
+        System.out.println(new SearchInsertPosition().searchInsert(i, 100));
     }
 }
