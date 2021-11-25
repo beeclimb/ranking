512677d6e3cee9dd90dab3043146d19cf844070a
SmarTest Demo User <demo@localhost.localdomain>
2021-11-25 14:01:54 +0800

    2021/11/25_1
    Change-Id: I4469c87e04028a22974bd0482634becfd19aa50e

--- src/test/java/org/option/AppTest.java
+++ src/test/java/org/option/AppTest.java
@@ -3,15 +3,15 @@ package org.option;
 import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
-import org.option.binarysearch.BinarySearch;
-import org.option.binarysearch.FirstBadVersion;
-import org.option.binarysearch.SearchInsertPosition;
+import org.option.doublepointer.SquaresOfaSortedArray;
+
+import java.util.Arrays;
 
 public class AppTest {
     @Test
     public void test() {
-        int[] i = {1, 3, 5, 7, 9};
+        int[] i = {-4,-1,0,3,10};
         int[] j = {1};
-        System.out.println(new SearchInsertPosition().searchInsert(i, 100));
+        System.out.println(Arrays.toString(new SquaresOfaSortedArray().sortedSquares2(i)));
     }
 }
