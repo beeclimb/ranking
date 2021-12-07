f40f3c5c9c62a8082e60113230cbc5f455d40ebd
root <root@localhost.localdomain>
2021-12-07 10:16:09 +0800

    2021/12/7 new 1
    Change-Id: I9de2a98ef2aa712ea9ce9254590e3d0c196646cd

--- src/test/java/org/option/AppTest.java
+++ src/test/java/org/option/AppTest.java
@@ -3,6 +3,7 @@ package org.option;
 
 import org.junit.Test;
 import org.option.doublepointer.MoveZeroes;
+import org.option.doublepointer.ReverseString;
 import org.option.doublepointer.TwoSumInputArrayIsSorted;
 
 import java.util.Arrays;
@@ -11,10 +12,9 @@ import java.util.Arrays;
 public class AppTest {
     @Test
     public void test() {
-        int[] i = {-1, 0};
-        int[] i1 = {2,7,11,15};
-        int[] i2 = {5,25,75};
-        System.out.println(Arrays.toString(new TwoSumInputArrayIsSorted().twoSum(i2, 100)));
+        char[] s = {'h','e','l','l','o'};
+        new ReverseString().reverseString(s);
+        System.out.println(Arrays.toString(s));
 
     }
 
