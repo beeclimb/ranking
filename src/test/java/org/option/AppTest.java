a3cea2dd1a19373f4854604578d37e363c9c1ecf
root <root@localhost.localdomain>
2021-12-07 16:14:04 +0800

    2021/12/7 new 2
    Change-Id: I77a43899ea856724439e1b060fc0833d25e78aa6

--- src/test/java/org/option/AppTest.java
+++ src/test/java/org/option/AppTest.java
@@ -4,6 +4,7 @@ package org.option;
 import org.junit.Test;
 import org.option.doublepointer.MoveZeroes;
 import org.option.doublepointer.ReverseString;
+import org.option.doublepointer.ReverseWordsInStringIII;
 import org.option.doublepointer.TwoSumInputArrayIsSorted;
 
 import java.util.Arrays;
@@ -12,9 +13,10 @@ import java.util.Arrays;
 public class AppTest {
     @Test
     public void test() {
-        char[] s = {'h','e','l','l','o'};
-        new ReverseString().reverseString(s);
-        System.out.println(Arrays.toString(s));
+        String s = "Let's take LeetCode contest";
+        System.out.println(new ReverseWordsInStringIII().reverseWords(s));
+        System.out.println(new ReverseWordsInStringIII().reverseWords2(s));
+
 
     }
 
