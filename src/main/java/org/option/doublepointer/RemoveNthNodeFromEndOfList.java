bb597b422f339ba533217c18d24eeb637719b3b1
root <root@localhost.localdomain>
2021-12-08 11:26:39 +0800

    2021/12/8 update 1
    Change-Id: I311bf2c9597323afb8b334c8a0aff21947cdd2a8

--- src/main/java/org/option/doublepointer/RemoveNthNodeFromEndOfList.java
+++ src/main/java/org/option/doublepointer/RemoveNthNodeFromEndOfList.java
@@ -26,4 +26,29 @@ public class RemoveNthNodeFromEndOfList {
         slow.next = slow.next.next;
         return head;
     }
+
+    public ListNode removeNthFromEnd2(ListNode head, int n) {
+        ListNode dummy = new ListNode(0, head);
+        ListNode slow = dummy;
+        ListNode fast = head;
+        for (int i = 0; i < n; i++) {
+            fast = fast.next;
+        }
+        while (fast != null) {
+            slow = slow.next;
+            fast = fast.next;
+        }
+        slow.next = slow.next.next;
+        return dummy.next;
+
+    }
+
 }
+
+
+
+
+
+
+
+
