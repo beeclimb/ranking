a18d6abbcc3cdd9225e188ec5b8f2260736ea416
jun.ma <1096596382@qq.com>
2021-12-08 22:43:55 +0800

    2021/11/8 update 2

---
+++ src/main/java/org/option/slidingwindow/LongestSubstringWithoutRepeatingCharacters.java
@@ -0,0 +1,13 @@
+package org.option.slidingwindow;
+
+/**
+ * @author Jun
+ * @date 2021/12/8
+ * @leetcode 3. Longest Substring Without Repeating Characters
+ */
+public class LongestSubstringWithoutRepeatingCharacters {
+    public int lengthOfLongestSubstring(String s) {
+
+
+    }
+}
