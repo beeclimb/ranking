020fba699988f994ad3d1b8f8a33e21bc0cef078
jun.ma <1096596382@qq.com>
2021-12-14 08:37:34 +0800

    2021/12/14 new 1

--- src/main/java/org/option/slidingwindow/PermutationInString.java
+++ src/main/java/org/option/slidingwindow/PermutationInString.java
@@ -1,5 +1,9 @@
 package org.option.slidingwindow;
 
+import java.awt.image.renderable.RenderableImage;
+import java.util.HashMap;
+import java.util.HashSet;
+
 /**
  * @author Jun
  * @date 2021/12/13
@@ -7,9 +11,29 @@ package org.option.slidingwindow;
  */
 public class PermutationInString {
     public boolean checkInclusion(String s1, String s2) {
-
-
-
-
+        HashSet<Character> set1 = new HashSet<>(s1.length());
+        for (int i = 0; i < s1.length(); i++) {
+            set1.add(s1.charAt(i));
+        }
+        int left = 0, right = 0;
+        Character rightValue;
+        while (left < s2.length()) {
+            while (right < left + s1.length() && right < s2.length()) {
+                rightValue = s2.charAt(right);
+                if (set1.contains(rightValue)) {
+                    set1.remove(rightValue);
+                    } else {
+                        set2.clear();
+                        ++left;
+                        break;
+                    }
+                } else {
+                    left = 2 * right - left + 1;
+                    break;
+                }
+            }
+            right = left;
+        }
+        return false;
     }
 }
--- src/test/java/org/option/AppTest.java
+++ src/test/java/org/option/AppTest.java
@@ -7,6 +7,7 @@ import org.option.doublepointer.ReverseString;
 import org.option.doublepointer.ReverseWordsInStringIII;
 import org.option.doublepointer.TwoSumInputArrayIsSorted;
 import org.option.slidingwindow.LongestSubstringWithoutRepeatingCharacters;
+import org.option.slidingwindow.PermutationInString;
 
 import java.util.Arrays;
 
@@ -14,8 +15,10 @@ import java.util.Arrays;
 public class AppTest {
     @Test
     public void test() {
-        String s = "aabaab!bb";
-        System.out.println(new LongestSubstringWithoutRepeatingCharacters().lengthOfLongestSubstring2(s));
+        String s1 =  "abcdxabcde";
+        String s2 =    "abcdeabcdx";
+
+        System.out.println(new PermutationInString().checkInclusion(s1, s2));
 
 
     }
