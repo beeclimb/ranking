cac6b2be66ba7612dff0317ed56fbc36530a1d03
jun.ma <1096596382@qq.com>
2021-12-24 08:26:56 +0800

    2021/12/24 add 1

--- src/main/java/org/option/depthbreadth/MaxAreaOfIsland.java
+++ src/main/java/org/option/depthbreadth/MaxAreaOfIsland.java
@@ -1,13 +1,29 @@
 package org.option.depthbreadth;
 
+import java.util.HashSet;
+
 /**
  * @author Jun
  * @date 2021/12/23
  * @leetcode 695. Max Area of Island
  */
 public class MaxAreaOfIsland {
+    int[] dx = {0, -1, 0, 1};
+    int[] dy = {1, 0, -1, 0};
+    public int maxAreaOfIsland(int[][] grid) {
+        int xLength = grid.length, yLength = grid[0].length;
+        HashSet<int[]> visitedIsland = new HashSet<>();
+
+    }
+
+    public void DFS(int[][] grid, int x, int y) {
+        if (grid[x][y] == 0) {
+
+        } else {
+
+        }
 
 
-
+    }
 
 }
