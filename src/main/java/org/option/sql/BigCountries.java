cf6e1bba46ee3b36f8296ada883c0d937abd8deb
M.jun <majun@lupuamc.com>
2022-06-24 19:04:55 +0800

    [sql] leetcode 183 584 1757

--- src/main/java/org/option/sql/BigCountries.java
+++ src/main/java/org/option/sql/BigCountries.java
@@ -6,8 +6,8 @@ package org.option.sql;
  * @leetcode 595. Big Countries
  */
 public class BigCountries {
-    String sql_595 = "select name, population, area from World where area >= 3000000 or population >= 25000000;";
-    String sql_595_2 =
+    String sql = "select name, population, area from World where area >= 3000000 or population >= 25000000;";
+    String sql_1 =
             "select name, population, area from World where area >= 3000000 \n" +
             "union\n" +
             "select name, population, area from World where population >= 25000000;";
---
+++ src/main/java/org/option/sql/RecyclableAndLowFatProducts.java
@@ -0,0 +1,10 @@
+package org.option.sql;
+
+/**
+ * @author jun.ma
+ * @date 2022/6/24 16:39:00
+ * @leetcode 1757. Recyclable and Low Fat Products
+ */
+public class RecyclableAndLowFatProducts {
+    String sql = "select product_id from Products where low_fats='Y' and recyclable='Y';";
+}
