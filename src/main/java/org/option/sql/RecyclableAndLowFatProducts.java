0c0abc6aa2fad35141a939a38da77821769b5ee1
jun.ma <juning.ma@qq.com>
2022-06-24 23:43:00 +0800

    [sql] 1757

--- src/main/java/org/option/sql/RecyclableAndLowFatProducts.java
+++ src/main/java/org/option/sql/RecyclableAndLowFatProducts.java
@@ -7,4 +7,8 @@ package org.option.sql;
  */
 public class RecyclableAndLowFatProducts {
     String sql = "select product_id from Products where low_fats='Y' and recyclable='Y';";
+    String sql_1 = "select c.Name as Customers \n" +
+                "from Customers as c\n" +
+                "left join Orders as o on c.Id = o.CustomerId\n" +
+                "where o.Id is null"
 }
