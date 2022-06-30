cad0f31a76aca3e0461fa62554589b8d2ff23a5b
jun.ma <juning.ma@qq.com>
2022-06-30 11:51:15 +0800

    [sql] 1693 1729 197 607 1141

---
+++ src/main/java/org/option/sql/FindFollowersCount.java
@@ -0,0 +1,13 @@
+package org.option.sql;
+
+/**
+ * @author jun.ma
+ * @date 2022/6/30 11:49:00
+ * @leetcode 1729. Find Followers Count
+ */
+public class FindFollowersCount {
+    String sql = "select user_id, count(follower_id) as followers_count\n" +
+            "from Followers\n" +
+            "group by user_id\n" +
+            "order by user_id;";
+}
