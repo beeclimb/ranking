b4bbbeb5bb561bcfe8c9033def7d75a058b9b6af
jun.ma <juning.ma@qq.com>
2022-07-12 13:29:11 +0800

    [Add] primary bit and sort

--- src/test/java/org/option/AppTest.java
+++ src/test/java/org/option/AppTest.java
@@ -2,47 +2,20 @@ package org.option;
 
 
 import org.junit.Test;
-import org.option.depthbreadth.FloodFill;
-import org.option.depthbreadth.MaxAreaOfIsland;
-import org.option.doublepointer.MoveZeroes;
-import org.option.doublepointer.ReverseString;
-import org.option.doublepointer.ReverseWordsInStringIII;
-import org.option.doublepointer.TwoSumInputArrayIsSorted;
-import org.option.slidingwindow.LongestSubstringWithoutRepeatingCharacters;
-import org.option.slidingwindow.PermutationInString;
-
-import java.util.Arrays;
 
 
 public class AppTest {
-    @Test
-    public void test() {
-        int[][] a = new int[][] {
-                {0,0,1,0,0,0,0,1,0,0,0,0,0},
-                {0,0,0,0,0,0,0,1,1,1,0,0,0},
-                {0,1,1,0,1,0,0,0,0,0,0,0,0},
-                {0,1,0,0,1,1,0,0,1,0,1,0,0},
-                {0,1,0,0,1,1,0,0,1,1,1,0,0},
-                {0,0,0,0,0,0,0,0,0,0,1,0,0},
-                {0,0,0,0,0,0,0,1,1,1,0,0,0},
-                {0,0,0,0,0,0,0,1,1,0,0,0,0}
-        };
-
-
-//        System.out.println(new MaxAreaOfIsland().maxAreaOfIsland3(a));
-        System.out.println(new MaxAreaOfIsland().maxAreaOfIsland2(a));
-
-    }
 
     @Test
-    public void fTest() {
-        System.out.println(f(5));
+    public void appTest() {
+        binary(3);
     }
 
-    public int f(int n) {
-        if (n == 1) {
-            return 1;
+    public static void binary(int number) {
+        for (int i = 31; i >= 0; --i) {
+
+            System.out.print((number & (1 << i)) == 0 ? "0" : "1");
         }
-        return f(n - 1) + 1;
     }
+
 }
