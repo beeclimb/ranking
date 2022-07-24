40d460c846d929bfc5568e0b045916dd6b01b26e
jun.ma <juning.ma@qq.com>
2022-07-24 22:49:35 +0800

    [LinkedList] reverse double linked list

--- src/main/java/github/beeclimb/preview/LinkedList.java
+++ src/main/java/github/beeclimb/preview/LinkedList.java
@@ -28,12 +28,35 @@ public class LinkedList {
         }
     }
 
+    public static class DoubleNode {
+        public int value;
+        public DoubleNode last;
+        public DoubleNode next;
+
+        public DoubleNode(int data) {
+            value = data;
+        }
+    }
+
     public static Node reverseLinkedList(Node head) {
         Node next = null;
         Node pre = null;
         while (head != null) {
             next = head.next;
             head.next = pre;
+            pre = head;
+            head = next;
+        }
+        return pre;
+    }
+
+    public static DoubleNode reverseDoubleList(DoubleNode head) {
+        DoubleNode pre = null;
+        DoubleNode next = null;
+        while (head != null) {
+            next = head.next;
+            head.next = pre;
+            head.last = next;
             pre = head;
             head = next;
         }
