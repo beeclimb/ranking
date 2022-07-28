dfeeb85f9b8c95a283be710aa7355f184ec04984
jun.ma <juning.ma@qq.com>
2022-07-28 22:41:22 +0800

    [LinkedList] leetcode 25

---
+++ src/main/java/github/beeclimb/linkedlist/ReverseNodesInkGroup25.java
@@ -0,0 +1,70 @@
+package github.beeclimb.linkedlist;
+
+/**
+ * @author jun.ma
+ * @date 2022/7/28 22:01:00
+ * @leetcode 25. Reverse Nodes in k-Group
+ */
+public class ReverseNodesInkGroup25 {
+
+    public static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    public ListNode reverseKGroup(ListNode head, int k) {
+        ListNode start = head;
+        ListNode end = getKGroupEnd(start, k);
+        if (end == null) {
+            return head;
+        }
+        head = end;
+        reverse(start, end);
+        ListNode lastEnd = start;
+        while (lastEnd.next != null) {
+            start = lastEnd.next;
+            end = getKGroupEnd(start, k);
+            if (end == null) {
+                return head;
+            }
+            reverse(start, end);
+            lastEnd.next = end;
+            lastEnd = start;
+        }
+        return head;
+    }
+
+    public static ListNode getKGroupEnd(ListNode start, int k) {
+        while (--k != 0 && start != null) {
+            start = start.next;
+        }
+        return start;
+    }
+
+    public static void reverse(ListNode start, ListNode end) {
+        end = end.next;
+        ListNode pre = null;
+        ListNode next = null;
+        ListNode cur = start;
+        while (cur != end) {
+            next = cur.next;
+            cur.next = pre;
+            pre = cur;
+            cur = next;
+        }
+        start.next = end;
+    }
+
+}
