e926a64278300a0a431a39ae7ec2a93d4a1feef0
jun.ma <juning.ma@qq.com>
2022-08-06 17:42:26 +0800

    [Preview] merge sort

---
+++ src/main/java/github/beeclimb/preview/MergeSort.java
@@ -0,0 +1,144 @@
+package github.beeclimb.preview;
+
+/**
+ * @author jun.ma
+ * @date 2022/8/5/ 11:29
+ */
+public class MergeSort {
+
+    // 非递归实现
+    public static void mergeSort1(int[] arr) {
+        if (arr == null || arr.length < 2) {
+            return;
+        }
+        process(arr, 0, arr.length - 1);
+    }
+
+    public static void process(int[] arr, int L, int R) {
+        if (L == R) {
+            return;
+        }
+        int M = L + ((R - L) >> 1);
+        process(arr, L, M);
+        process(arr, M + 1, R);
+        merge(arr, L, M, R);
+    }
+
+    public static void merge(int[] arr, int L, int M, int R) {
+        int[] help = new int[R - L + 1];
+        int i = 0;
+        int p1 = L;
+        int p2 = M + 1;
+        // p1,p2 都没越界
+        while (p1 <= M && p2 <= R) {
+            help[i++] = arr[p1] <= arr[p2] ? arr[p1++] : arr[p2++];
+        }
+        while (p1 <= M) {
+            help[i++] = arr[p1++];
+        }
+        while (p2 <= R) {
+            help[i++] = arr[p2++];
+        }
+        for (i = 0; i < help.length; ++i) {
+            arr[L + i] = help[i];
+        }
+    }
+
+    // for test
+    public static int[] generateRandomArray(int maxSize, int maxValue) {
+        int[] arr = new int[(int) ((maxSize + 1) * Math.random())];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = (int) ((maxValue + 1) * Math.random()) - (int) (maxValue * Math.random());
+        }
+        return arr;
+    }
+
+    // for test
+    public static int[] copyArray(int[] arr) {
+        if (arr == null) {
+            return null;
+        }
+        int[] res = new int[arr.length];
+        for (int i = 0; i < arr.length; i++) {
+            res[i] = arr[i];
+        }
+        return res;
+    }
+
+    // for test
+    public static boolean isEqual(int[] arr1, int[] arr2) {
+        if ((arr1 == null && arr2 != null) || (arr1 != null && arr2 == null)) {
+            return false;
+        }
+        if (arr1 == null && arr2 == null) {
+            return true;
+        }
+        if (arr1.length != arr2.length) {
+            return false;
+        }
+        for (int i = 0; i < arr1.length; i++) {
+            if (arr1[i] != arr2[i]) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    // for test
+    public static void printArray(int[] arr) {
+        if (arr == null) {
+            return;
+        }
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
+
+    // for test
+    static void selectionSort(int[] array) {
+        if (array == null || array.length < 2) {
+            return;
+        }
+        int n = array.length;
+        // 确定这次排哪个位置的数
+        for (int i = 0; i < n - 1; ++i) {
+            // 最小数的指针
+            int minIndex = i;
+            // 从要排的那个数后一位，开始往后遍历，确定出最小数的位置
+            for (int j = i + 1; j < n; ++j) {
+                minIndex = array[minIndex] < array[j] ? minIndex : j;
+            }
+            swap(array, i, minIndex);
+        }
+    }
+
+    // for test
+    static void swap(int[] arr, int i, int j) {
+        int tmp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = tmp;
+    }
+
+    // for test
+    public static void main(String[] args) {
+        int testTime = 500000;
+        int maxSize = 100;
+        int maxValue = 100;
+        System.out.println("测试开始");
+        for (int i = 0; i < testTime; i++) {
+            int[] arr1 = generateRandomArray(maxSize, maxValue);
+            int[] arr2 = copyArray(arr1);
+            mergeSort1(arr1);
+            selectionSort(arr2);
+            if (!isEqual(arr1, arr2)) {
+                System.out.println("出错了！");
+                printArray(arr1);
+                printArray(arr2);
+                break;
+            }
+        }
+        System.out.println("测试结束");
+    }
+
+}
